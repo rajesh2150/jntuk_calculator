e66ea2d781147f2a14d558ef921378149717eccb
Rajesh Korlapati <77844866+rajesh2150@users.noreply.github.com>
2022-07-27 21:17:07 +0530

    Update final.py

--- final.py
+++ final.py
@@ -1,25 +1,24 @@
-# Online Python compiler (interpreter) to run Python online.
-# Write Python 3 code in this online editor and run it.
+
 print("Welcome to SPGA-CGPA Calculator")
-type1=int(input("enter 1.for cgpa 2.for sgpa:"))
+type1=int(input("Enter 1.For Percentage 2.For Ponits:"))
 if type1==1:
-    cgpa=float(input("enter cgpa:"))
-    if cgpa > 0 and cgpa <= 10:
+    cgpa=float(input("Enter CGPA:"))
+    if cgpa > 0 and cgpa <= 10.75:
         percentage=cgpa-0.75
         temp=percentage*10
         print('----------------------------------')
-        print ("you have percentage:",temp,'%')
+        print ("You have percentage:",temp,'%')
         print('----------------------------------')
         print("Thank You.......")
     else:
         print('Invalid cgpa')
 elif type1==2:
-    spga=float(input('Enter your sgpa:'))
+    spga=float(input('Enter your SPGA:'))
     if spga > 0 and spga <= 100:
         temp1=spga/10
         points=temp1 + 0.75
         print('----------------------------------')
-        print ("you have CGPA:",points,'points')
+        print ("You have CGPA:",points,'points')
         print('----------------------------------')
         
         print("Thank You.......")
@@ -28,4 +27,4 @@ elif type1==2:
 else:
     print('Invalid Option Enterd')
     print("Thank You")
-print('Made By Rajesh korlapati')
+print('Made By ❤ Rajesh korlapati')
